bfda56d227ea5fc09ab9070ae99d52a7126971f9
saritayadav <sarita.yadav13@outlook.com>
2026-01-31 16:31:59 +0100

    bootstrap Spring Boot app with H2 configuration and health endpoint

---
+++ src/main/java/com/example/todos/simpletodoservice/SimpleTodoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.todos.simpletodoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTodoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTodoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/todos/simple_todo_service/SimpleTodoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todos.simple_todo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTodoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
